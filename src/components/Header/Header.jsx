03753094c049e79c8affbf43e8cfeb6868a09e51
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-02-28 10:54:08 +0100

    update/store_folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  // const numberCart = useSelector(state => state.numberItems);
+  const numberCart = useSelector(state => state.numberItems);
   return (
     <header>
       <Link to="/"><h1>Store</h1></Link>
